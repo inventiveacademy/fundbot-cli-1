752f373f9dec1c82cc97d59dc697d919066a9854
Trinity Siemer <tsiemer@gmail.com>
2017-08-31 14:53:59 -0500

    Added request

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
 var program = require('commander');
+var request = require('request');
  
 program
   .version('0.1.0')
@@ -7,26 +8,35 @@ program
   .option('-c, --postApplicants', 'Creating Applicants')
   .option('-d, --delApplicant', 'deleting Applicant')
   .parse(process.argv);
- 
+
+console.log('You have applicants:');
+if (program.getApplicants)
+	getApplicants();
+
+if (program.getApplicant) 
+	getApplicant();
+
+if (program.postApplicants) 
+	postApplicants();
+
+if (program.delApplicant) 
+	delApplicant();
+
 function getApplicants(){
+	request('localhost:3001/applicants', function (error, response, body) {
+		console.log(body);
+	});
 	console.log("Getting all Applicants Master!!");
 }
 
 function getApplicant(){
 	console.log("Getting the Applicant!!");
 }
-console.log('You have applicants:');
 
-if (program.getApplicants) 
-	console.log('  - getApplicants');
-	getApplicants();
+function postApplicants(){
+	console.log("Creating a new Applicant!!");
+}
 
-if (program.getApplicant) 
-	console.log('  - getApplicant');
-	getApplicant();
-
-if (program.postApplicants) 
-	console.log('  - postApplicants');
-
-if (program.delApplicant) 
-	console.log('  - delApplicant');
+function delApplicant(){
+	console.log("Deleting an Applicant!!");
+}
